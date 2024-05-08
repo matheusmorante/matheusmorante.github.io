6e0a60d1ad16f644ed93a0fd62710a1853ccc5fa
Morante <112764388+matheusmorante@users.noreply.github.com>
2024-05-08 14:10:13 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,9 @@ var menu = document.getElementById('menu');
 
 // Função para ocultar ou exibir a lista de navegação
 function hiddenNavList() {
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none'
-        console.log('Lista de navegação escondida')
-    } 
-    else {
-        menu.style.display = 'block'
-        console.log('Lista de navegação exibida')
-    }
+
+    menu.classList.toggle('menu-opened')
+ 
 }
 
 // Adiciona um ouvinte de evento de clique ao ícone do menu
