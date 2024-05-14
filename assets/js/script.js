e856e1ac4044373cd83990834dfa2bb8b56cf570
Morante <112764388+matheusmorante@users.noreply.github.com>
2024-05-14 14:28:02 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,44 @@
 const menuIcon = document.querySelector('#menu-icon');
 const menu = document.querySelector('#menu');
 const header = document.querySelector('#header');
-const menuContainer = document.querySelector('#menu-container')
+const overlay = document.querySelector('#overlay');
+const projectModal = document.querySelector('#project-modal')
+
 
 // Função para ocultar ou exibir a lista de navegação
 function openMenu() {
+    toogleOverlay();
     menu.classList.toggle('open-menu');
     menuIcon.classList.toggle('bi-x');
-    menuContainer.classList.toggle('overlay');
+}
+
+function toogleOverlay() {
+    overlay.classList.toggle('overlay-bg');
     document.body.classList.toggle('overflowHidden')
 }
 
 // Adiciona um ouvinte de evento de clique ao ícone do menu
-document.addEventListener('click', (e) => {
-    const targetEL = e.target;
+document.addEventListener('click', e => {
+    const targetEl = e.target;
 
-    if (targetEL.id === 'menu-icon') {
+    if (targetEl.id === 'menu-icon' || targetEL.classList.contains('menu-item')) {
         openMenu()
-     }
-
-    if (targetEL.classList.contains('menu-item')) {
-       openMenu()
     }
-    console.log(4)
+
+    if (targetEl.classList.contains('projects')) {
+        const title = targetEl.querySelector('#modal-title');
+        const image = targetEl.querySelector('project-modal-image')
+        const testUrl = targetEl.querySelector('test-project');
+        const codeUrl = targetEl.querySelector('see-code');
+        const description = targetEl.querySelector('description');
+
+        if (targetEl.id === 'calculator-project') {
+            title.innerText = 'Calculadora';
+            image.src = 'assets\images\project_card_background\calculadora.png';
+            testUrl.href = 'projects/calculator.index';
+            codeUrl.href = 'https://github.com/matheusmorante/matheusmorante.github.io/tree/f41da9c97987320ee64ea2d2008afaa149a117de/projects/calculadora';
+            description.innerText =
+                'Esse projeto foi muito importante para a consolidação do meu aprendizado sobre'
+        }
+    }
 })
